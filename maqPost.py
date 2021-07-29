d8f473123acac8e0e54b290e6fa58d90a0e8d3dc
Nikollas <nikollasferreirag@gmail.com>
2021-07-29 08:44:19 -0300

    anbn funcional e correções na mp

--- maqPost.py
+++ maqPost.py
@@ -12,6 +12,8 @@ class Arquivo:
     def ler_arquivo(self):
         with open(self.arquivo) as f:
             for line in f:
+                if line.__contains__(";"):
+                    continue
                 if not line.rstrip() == "":
                     palavras = line.rstrip().split(" ")
                     index = int(palavras.pop(0))
@@ -25,7 +27,7 @@ class MP:
     estado: int
     func: str
     char: chr
-    prox_estado: int
+
     final: bool
     fila: deque
     accept: bool
@@ -34,13 +36,12 @@ class MP:
         return '''Estado: {}
 Função: {}
 Caractere: {}
-Próximo estado: {}'''.format(self.estado, self.func, self.char, self.prox_estado)
+'''.format(self.estado, self.func, self.char)
 
     def __init__(self, estado: int, fila: deque, func: str = "", char: chr = ""):
         self.estado = estado
         self.func = func
         self.char = char
-        # self.prox_estado = estado + 1
         self.fila = fila
         self.final = False
 
@@ -58,21 +59,20 @@ Próximo estado: {}'''.format(self.estado, self.func, self.char, self.prox_estad
 
     def __ler(self):
         self.char = self.fila.popleft()
-        self.prox_estado = self.estado + 1
+        self.estado += 1
 
     def __jump(self, param):
         # caça no arquivo até encontrar instrução do estado
         # caso nao encontre, verificar novamente o mesmo estado até acabar ou gerar erro
         if self.char == param[0]:
-            self.prox_estado = int(param[1])
+            self.estado = int(param[1])
             self.char = ""
         else:
-            self.prox_estado = self.estado + 1  # int(param[1]) + 1
-
+            self.estado += 1
 
     def __adiciona(self, param):
         self.fila.append(param)
-        self.prox_estado = self.estado + 1
+        self.estado += 1
 
     def __rejeita(self):
         print("Cadeia rejeitada")
@@ -94,31 +94,18 @@ Próximo estado: {}'''.format(self.estado, self.func, self.char, self.prox_estad
 # 0 estado de aceitação
 # -1 estao de rejeição
 
-
-# Não é meu
-# push S (add S na fila)
-# jr S (encerra programa e rejeita se o símbolo lido da ponta de saída é S)
-# ja S (encerra programa e aceita se o símbolo lido da ponta de saída é S)
-# jmp E, S (desvia fluxo de execução caso a )
-# read (lê símbolo da ponta de saída e armazena numa variável temporária pra fazer todos os testes desses S)
 if __name__ == '__main__':
-    cadeia = "aabb"
-    a = Arquivo("teste.txt")
+    cadeia = "aaaabbbb"
+    a = Arquivo("anbnMP.txt")
     a.ler_arquivo()
     print(cadeia_funcoes)
     q = deque()
     for c in cadeia:
         q.append(c)
-    #
     mp = MP(1, q)
-    # print(cadeia_funcoes.get(1)[0])
-    mp.func = cadeia_funcoes.get(1)[0]
-    mp.funcao(cadeia_funcoes.get(1)[1:])
     while not mp.final:
-        mp.estado = mp.prox_estado
         mp.func = cadeia_funcoes.get(mp.estado)[0]
         mp.funcao(cadeia_funcoes.get(mp.estado)[1:])
-
 
 # print(q)
 # mp.func = "read"
@@ -130,16 +117,7 @@ if __name__ == '__main__':
 #     mp.func = mp.func.split(" ")[0]
 #     mp.funcao("a")
 
-# ; programa que reconhece cadeias vazias ou com número par de 1's
-# 11: push #
-# 12: read
-# 13: jr 0
-# 15: ja #
-# 16: jmp 17, 1
-# 17: read
-# 18: jr 0
-# 20: jr #
-# 21: jmp 12, 1
+
 
 
 # escreve #
